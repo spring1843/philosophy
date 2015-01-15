c61bffe766dc92ed02804f2504418edd6a015f5d
Yasser Abdolmaleki <yasser@yasser.ca>
2015-01-15 03:23:21 -0500

    Most tree features implemented

--- modules/pathToPhilosophy.js
+++ modules/pathToPhilosophy.js
@@ -1,28 +1,109 @@
-module.exports = exports = function (di) {
+PathToPhilosophy = function (di) {
 
-    var scraper = require('./scraper');
+    var dep = di;
+    var scraper = dep.scraper || require('./scraper');
+    var destinationUrl = dep.destination || 'http://en.wikipedia.org/wiki/Philosophy';
+    var path = [];
+    var visits = [];
+    var root = null;
+    var scrapes = 0;
+    var maxDepth = dep.maxDepth || 6;
+    var maxNumberOfVisits = null;
 
-    var dep = {};
+    var find = function (url, callback) {
+        console.log('Scraping', url);
+        recursiveFind(url, callback);
+    }
 
-    var linksToPhilosophy = function (wikiPageLink) {
-        if (wikiPageLink.children.indexOf('http://en.wikipedia.org/wiki/Philosophy') != -1) {
+    var recursiveFind = function (url, callback) {
+
+
+        checkForMaxVisits(callback);
+        scraper.getWikiPageLinks(url, function (wikiPageLink) {
+
+            handleRoot(wikiPageLink, callback);
+
+            if (hasChildren(wikiPageLink) === false) {
+                return false;
+            }
+
+            if (checkForSuccess(wikiPageLink, callback) === true)
+                return true;
+            depthFirstSearch(wikiPageLink, callback);
+        });
+    }
+
+    var hasChildren = function (wikiPageLink) {
+        if (wikiPageLink.children && wikiPageLink.children.length > 0)
             return true;
-        } else {
-            return false;
+        return false;
+    }
+
+
+    var handleRoot = function (wikiPageLink, callback) {
+        if (path.length === 0) {
+            path.push(wikiPageLink.link);
+            root = wikiPageLink;
+            if (hasChildren(root) === true) {
+                maxNumberOfVisits = root.children.length * maxDepth;
+            } else {
+                callback(null);
+            }
         }
     }
 
-    var find = function (url, callback) {
-        console.log("scraping", url);
-        scraper.getWikiPageLinks(url, function (wikiPageLink) {
+    var checkForSuccess = function (wikiPageLink, callback) {
+        console.log('is linked to Philosophy?', isLinkedToPhilosophy(wikiPageLink));
+        if (isLinkedToPhilosophy(wikiPageLink)) {
+            callback(path);
+            console.log("FOUND PATH!", path);
+            return true;
+        }
+        return false;
+    }
 
-            console.log("links to?", linksToPhilosophy(wikiPageLink));
-            if (linksToPhilosophy(wikiPageLink))
-                callback(wikiPageLink);
-        });
+    var isLinkedToPhilosophy = function (wikiPageLink) {
+        if (wikiPageLink.children.indexOf(destinationUrl) != -1)
+            return true;
+        else
+            return false;
+    }
+
+    var checkForMaxVisits = function (callback) {
+        if (++scrapes == maxNumberOfVisits)
+            callback(null);
+    }
+    var checkForMxaxDepth = function () {
+        if (path.length < maxDepth)
+            return true;
+        return false;
+    }
+
+    var depthFirstSearch = function (wikiPageLink, callback) {
+        for (var i in wikiPageLink.children) {
+            if (checkForMxaxDepth()) {
+                var child = wikiPageLink.children[i];
+                visitChild(child, callback);
+            }
+        }
+    }
+
+    var hasVisitedChild = function (childLink) {
+        if (visits.indexOf(childLink) === -1)
+            return false
+        return true;
+    }
+    var visitChild = function (childLink, callback) {
+        if (hasVisitedChild(childLink) === true)
+            return;
+
+        path.push(childLink);
+        visits.push(childLink);
+        recursiveFind(childLink, callback);
+        path.pop();
     }
 
     return {
         find: find
     };
-}();
+};
--- spec/pathToPhilosophySpec.js
+++ spec/pathToPhilosophySpec.js
@@ -1,15 +1,148 @@
 describe("PathToPhilosophy", function () {
     var pathToPhilosophy;
+
     beforeEach(function () {
-        pathToPhilosophy = require('../modules/pathToPhilosophy.js');
+        var di = {};
+        di.maxDepth = 4;
+        di.scraper = {};
+        //di.console = {log : function(data){ return; }};
+        di.scraper.getWikiPageLinks = function (url, callback) {
+
+            var data = {
+                link: "http://en.wikipedia.org/wiki/NoChildren",
+                children: []
+            };
+
+            if (url == "http://en.wikipedia.org/wiki/OneHop") {
+                data = {
+                    link: "http://en.wikipedia.org/wiki/OneHop",
+                    children: [
+                        "http://en.wikipedia.org/wiki/DummyPage1",
+                        "http://en.wikipedia.org/wiki/DummyPage2",
+                        "http://en.wikipedia.org/wiki/Philosophy"
+                    ]
+                };
+            }
+
+            if (url == "http://en.wikipedia.org/wiki/TwoHops") {
+                data = {
+                    link: "http://en.wikipedia.org/wiki/TwoHops",
+                    children: [
+                        "http://en.wikipedia.org/wiki/DummyPage1",
+                        "http://en.wikipedia.org/wiki/DummyPage1",
+                        "http://en.wikipedia.org/wiki/OneHop"
+                    ]
+                };
+            }
+
+            if (url == "http://en.wikipedia.org/wiki/ThreeHops") {
+                data = {
+                    link: "http://en.wikipedia.org/wiki/ThreeHops",
+                    children: [
+                        "http://en.wikipedia.org/wiki/DummyPage1",
+                        "http://en.wikipedia.org/wiki/TwoHops",
+                        "http://en.wikipedia.org/wiki/DummyPage1"
+                    ]
+                };
+            }
+
+            if (url == "http://en.wikipedia.org/wiki/FourHops") {
+                data = {
+                    link: "http://en.wikipedia.org/wiki/FourHops",
+                    children: [
+                        "http://en.wikipedia.org/wiki/ThreeHops",
+                        "http://en.wikipedia.org/wiki/DummyPage1",
+                        "http://en.wikipedia.org/wiki/DummyPage1"
+                    ]
+                };
+            }
+
+            if (url == "http://en.wikipedia.org/wiki/FiveHops") {
+                data = {
+                    link: "http://en.wikipedia.org/wiki/FiveHops",
+                    children: [
+                        "http://en.wikipedia.org/wiki/FourHops",
+                        "http://en.wikipedia.org/wiki/DummyPage1",
+                        "http://en.wikipedia.org/wiki/FourHops"
+                    ]
+                };
+            }
+
+            if (url == "http://en.wikipedia.org/wiki/Loopy1") {
+                data = {
+                    link: "http://en.wikipedia.org/wiki/Loopy1",
+                    children: [
+                        "http://en.wikipedia.org/wiki/Loopy2"
+                    ]
+                };
+            }
+
+            if (url == "http://en.wikipedia.org/wiki/Loopy1") {
+                data = {
+                    link: "http://en.wikipedia.org/wiki/Loopy2",
+                    children: [
+                        "http://en.wikipedia.org/wiki/Loopy1"
+                    ]
+                };
+            }
+            callback(data)
+
+        }
+        pathToPhilosophy = new PathToPhilosophy(di);
     });
 
-    it("Should be able to find path from Philosophy of life", function () {
-        var url = "http://en.wikipedia.org/wiki/Philosophy_of_life"
-        var path = pathToPhilosophy.find(url, function (path) {
-            console.log("path", path);
+    it("Should not be able to find a path when no children", function () {
+        var url = "http://en.wikipedia.org/wiki/NoChildren"
+        pathToPhilosophy.find(url, function (path) {
+            expect(path).toEqual(null);
+        });
+    });
+
+    it("Should be able to find path with 1 hop", function () {
+        var url = "http://en.wikipedia.org/wiki/OneHop"
+        pathToPhilosophy.find(url, function (path) {
             expect(path.length).toEqual(1);
         });
-
     });
+
+    it("Should be able to find path with 2 hops", function () {
+        var url = "http://en.wikipedia.org/wiki/TwoHops"
+        pathToPhilosophy.find(url, function (path) {
+            console.log("path", path);
+            expect(path.length).toEqual(2);
+        });
+    });
+
+    it("Should be able to find path with 3 hops", function () {
+        var url = "http://en.wikipedia.org/wiki/ThreeHops"
+        pathToPhilosophy.find(url, function (path) {
+            console.log("path", path);
+            expect(path.length).toEqual(3);
+        });
+    });
+
+    it("Should be able to find path with 4 hops", function () {
+        var url = "http://en.wikipedia.org/wiki/FourHops"
+        pathToPhilosophy.find(url, function (path) {
+            console.log("path", path);
+            expect(path.length).toEqual(4);
+        });
+    });
+
+    it("Should not be able to find a path after a maximum of 4 levels", function () {
+        var url = "http://en.wikipedia.org/wiki/FiveHops"
+        pathToPhilosophy.find(url, function (path) {
+            console.log("path", path);
+            expect(path).toEqual(null);
+        });
+    });
+
+    it("Should avoid getting stock in a loop", function () {
+        var url = "http://en.wikipedia.org/wiki/Loopy1"
+        pathToPhilosophy.find(url, function (path) {
+            console.log("path", path);
+            expect(path).toEqual(null);
+        });
+    });
+
 });
