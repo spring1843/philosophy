60b313cc2188b0b929760a95766c51a6a4e3452f
Yasser Abdolmaleki <yasser@yasser.ca>
2015-01-15 23:02:41 -0500

    before making path async compatible

--- models/WikiPageLink.js
+++ models/WikiPageLink.js
@@ -1,11 +1,8 @@
 module.exports.inject = function (di) {
     var dep = di;
     var wikiPageLinkSchema = dep.wikiPageLinkSchema || require('./schemas/wikiPageLink.js');
-
     var mongoose = dep.mongoose;
     var wikiPageLinkModel =  new mongoose.Schema(wikiPageLinkSchema);
-
-
     var WikiPageLink = mongoose.model('WikiPageLink',wikiPageLinkModel);
 
     return WikiPageLink;
